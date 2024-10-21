df900821932ef7d84183cf9e9a39b89090e56822
NitheeshSNB <nitheeshsnb@gmail.com>
2024-10-21 14:23:06 +0000

    Added smart contract key

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -6,7 +6,7 @@ import { ethers } from 'ethers';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0x573E5b7CF5340936D5406E9046400D17f3FF628c');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
